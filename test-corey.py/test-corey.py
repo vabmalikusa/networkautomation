9f5263cbd46a2f3ae4e8cb555b98cf031b8cea61
vaibhav.malik <vaibhav.malik@centurylink.com>
2021-06-07 15:33:10 -0500

    first commit

---
+++ test-corey.py/test-corey.py
@@ -0,0 +1,23 @@
+
+
+
+def decorator_function(original_function):
+    def wrapper_function(*args, **kwargs):
+        print('wrapper executed this before {}'.format(original_function.__name__))
+        return original_function(*args, **kwargs)
+    return wrapper_function
+
+@decorator_function
+def display():
+    print('display function ran')
+
+
+
+@decorator_function
+def display_info(name, age):
+    print('display_info ran with arguments ({}, {})'.format(name, age))
+
+display_info('John', 25)
+
+display()
+
