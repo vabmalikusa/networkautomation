8748b5ff32f4e37de0c11124b9f51402c6d2a02e
Vaibhav Malik <vaibhav.malik@centurylink.com>
2021-06-10 10:00:00 -0500

    Create terminal_server_variant2.py

---
+++ terminal_server_variant2.py
@@ -0,0 +1,21 @@
+from netmiko import ConnectHandler
+import logging
+logging.basicConfig(filename='netmiko_global.log', level=logging.DEBUG)
+logger = logging.getLogger("netmiko")
+device123 = {
+    "device_type": "terminal_server",
+    "ip": "192.168.29.141",
+    "username": "network-automation",
+    "password": "cisco",
+}
+ssh123 = ConnectHandler(**device123)
+output123 = ssh123.send_command("ifconfig")
+print(output123)
+#cisco
+ip123 = [" 192.168.29.133"]
+for xyz123 in ip123:
+    usernamer123 = input("Enter the username for " + xyz123 + ":")
+    ssh123.write_channel("ssh -l " + usernamer123 + xyz123 + "\n")
+    ssh123.write_channel(input("enter the password for ") + "\n")
+    output124 = ssh123.send_command("show ip int br")
+    print(output124)
